a2f9ff40da658475a2e53db69bbad89f9317885e
chenchuan <chenchuan@appledeMacBook-Pro.local>
2017-03-14 10:06:34 +0800

    Add something

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
-import Nav from './components/Nav'
+import Nav from './components/Nav';
+import Gallery from './components/Gallery';
+
+import './styles/App.css';
 
 class App extends Component {
   render() {
     return (
       <div>
         <Nav></Nav>
+        <Gallery></Gallery>
       </div>
     );
   }
---
+++ src/components/Gallery.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import '../styles/gallery.css';
+
+class Gallery extends Component {
+  render() {
+    return (
+      <div className="gallery">
+          <img src="http://imglf2.ph.126.net/J0MTgx7UULkDMXuFbR844g==/1056094112635378262.jpg"/>
+      </div>
+    );
+  }
+}
+
+export default Gallery;
