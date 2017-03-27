ab69e891affd25341fc56a9405f8efe60e1e3744
chenchuan <chenchuan@wosai-inc.com>
2017-03-27 14:32:55 +0800

    Add

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 import '../styles/gallery.css';
-import gallery from '../images/cakes.jpg'
+
 class Gallery extends Component {
   render() {
     return (
       <div className="gallery">
-          <img src={gallery}/>
+          <img src="http://upload-images.jianshu.io/upload_images/3261015-7c292fa446aceb63.jpg?imageMogr2/auto-orient/strip%7CimageView2/2/w/1240" alt="/" />
       </div>
     );
   }
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,17 +1,14 @@
 import React, { Component } from 'react';
 import '../styles/nav.css';
-import icon from '../images/icon.png';
 
 class Nav extends Component {
   render() {
     return (
-      <div className="nav">
-          <ul>
-              <li><img src={icon} className="icon" alt=""/></li>
-              <li><a href="#">主页</a></li>
-              <li><a href="#">产品</a></li>
-              <li><a href="#">关于</a></li>
-          </ul>
+      <div>
+        <div className="nav">
+          <a href='#'>端正果蔬园</a>
+        </div>
+        <div className="nav-clear"></div>
       </div>
     );
   }
